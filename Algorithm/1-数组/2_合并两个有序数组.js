f4aea52360aa497f5a4183bb6248471225b6fb0c
zjx <2252474825@qq.com>
2023-01-15 12:43:27 +0800

    feat: 数组算法练习

---
+++ Algorithm/1-数组/2_合并两个有序数组.js
@@ -0,0 +1,24 @@
+var merge = function (nums1, m, nums2, n) {
+    let i = m - 1
+    let j = n - 1
+    let k = m + n - 1
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] >= nums2[j]) {
+            nums1[k] = nums1[i]
+            k--
+            i--
+        } else {
+            nums1[k] = nums2[j]
+            k--
+            j--
+        }
+
+    }
+    while (j >= 0) {
+        nums1[k] = nums2[j]
+        k--
+        j--
+    }
+    return nums1
+}
+console.log(merge([1, 2, 3, 0, 0, 0],3,[2,5,6],3))
