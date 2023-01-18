499050daf814553cb053e0e464ceba19f117e69f
zjx <2252474825@qq.com>
2023-01-19 02:15:19 +0800

    feat: 有效括号的更新

--- Algorithm/3-栈/1_有效的括号.js
+++ Algorithm/3-栈/1_有效的括号.js
@@ -2,19 +2,38 @@
  * @param {string} s
  * @return {boolean}
  */
+// var isValid = function (s) {
+//     const leftToRight = {
+//         "{": "}",
+//         "[": "]",
+//         "(": ")"
+//     }
+//     if (!s) return true
+//     let stack = []
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === "{" || s[i] === "[" || s[i] === "(") {
+//             stack.push(leftToRight[s[i]])
+//         } else {
+//             if (stack.pop() !== s[i]) return false
+//         }
+//     }
+//     return !stack.length
+// };
+// 我更喜欢下面这种
 var isValid = function (s) {
-    const leftToRight = {
-        "{": "}",
+    const stack = []
+    const obj = {
+        "(": ")",
         "[": "]",
-        "(": ")"
+        "{": "}"
     }
-    if (!s) return true
-    let stack = []
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === "{" || s[i] === "[" || s[i] === "(") {
-            stack.push(leftToRight[s[i]])
+        if (s[i] in obj) {
+            stack.push(s[i])
         } else {
-            if (stack.pop() !== s[i]) return false
+            if (obj[stack.pop()] !== s[i]) {
+                return false
+            }
         }
     }
     return !stack.length
