1f919198f87b6db9ac55db6d98d464c1fba2514e
zjx <2252474825@qq.com>
2023-01-22 16:17:10 +0800

    feat: 移除元素

---
+++ Algorithm/1-数组/5_移除元素.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let slow = 0, fast = 0
+    while (fast < nums.length) {
+        if (nums[fast] !== val) {
+            nums[slow] = nums[fast]
+            slow++
+        }
+        fast++
+    }
+    return slow
+};
