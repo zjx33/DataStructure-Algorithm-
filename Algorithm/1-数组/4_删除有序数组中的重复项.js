5d3da20f7b58e9b9bc3dd9805b20d7c022769864
zjx <2252474825@qq.com>
2023-01-22 16:16:25 +0800

    feat: 删除有序数组中的重复项

---
+++ Algorithm/1-数组/4_删除有序数组中的重复项.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let slow = 0, fast = 0
+    while (fast < nums.length) {
+        if (nums[fast] !== nums[slow]) {
+            slow++
+            nums[slow] = nums[fast]
+        }
+        fast++
+    }
+    return slow + 1
+};
