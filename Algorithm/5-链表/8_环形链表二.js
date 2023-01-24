c7864add0a053809a7f7879cf72a9b5cf68c3ab3
zjx <2252474825@qq.com>
2023-01-24 12:15:11 +0800

    feat: 环形链表二更新

--- Algorithm/5-链表/8_环形链表二.js
+++ Algorithm/5-链表/8_环形链表二.js
@@ -24,19 +24,35 @@ var detectCycle = function (head) {
     // }
     // return null
     // 第二种快慢指针
-    let pre = head
-    let cur = head
-    if (head === null || head.next === null) return null
-    while (cur && cur.next) {
-        pre = pre.next
-        cur = cur.next.next
-        if (pre === cur) break
+    // let pre = head
+    // let cur = head
+    // if (head === null || head.next === null) return null
+    // while (cur && cur.next) {
+    //     pre = pre.next
+    //     cur = cur.next.next
+    //     if (pre === cur) break
+    // }
+    // if (pre !== cur) return null
+    // let p1 = head
+    // while (p1 !== pre) {
+    //     p1 = p1.next
+    //     pre = pre.next
+    // }
+    // return p1
+    if (!head) return null
+    let slow = head
+    let fast = head
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (slow === fast) {
+            let cur = head
+            while (cur !== slow) {
+                cur = cur.next
+                slow = slow.next
+            }
+            return cur
+        }
     }
-    if (pre !== cur) return null
-    let p1 = head
-    while (p1 !== pre) {
-        p1 = p1.next
-        pre = pre.next
-    }
-    return p1
+    return null
 };
