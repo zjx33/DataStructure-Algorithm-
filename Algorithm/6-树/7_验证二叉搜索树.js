b217461dc2157247c1a60ac863d12bb7f4a803dd
zjx <2252474825@qq.com>
2023-01-27 00:34:42 +0800

    feat: 更新二叉搜索树

--- Algorithm/6-树/7_验证二叉搜索树.js
+++ Algorithm/6-树/7_验证二叉搜索树.js
@@ -11,10 +11,20 @@
  * @return {boolean}
  */
 var isValidBST = function (root) {
-    function dfs(root, minValue, maxValue) {
+    // function dfs(root, minValue, maxValue) {
+    //     if (!root) return true
+    //     if (root.val >= maxValue || root.val <= minValue) return false
+    //     return dfs(root.left, minValue, root.val) && dfs(root.right, root.val, maxValue)
+    // }
+    // return dfs(root, -Infinity, Infinity)
+    let prev = -Infinity
+    function dfs(root) {
         if (!root) return true
-        if (root.val >= maxValue || root.val <= minValue) return false
-        return dfs(root.left, minValue, root.val) && dfs(root.right, root.val, maxValue)
+        let left = dfs(root.left)
+        if (root.val <= prev) return false
+        prev = root.val
+        let right = dfs(root.right)
+        return left && right
     }
-    return dfs(root, -Infinity, Infinity)
+    return dfs(root)
 };
