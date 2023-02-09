dbb7b2711b35bf4dd3ec3309c911667f12c06495
zjx <2252474825@qq.com>
2023-02-09 15:36:12 +0800

    feat: 冒泡排序

---
+++ Algorithm/12-排序/1_冒泡排序.js
@@ -0,0 +1,11 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+            }
+        }
+    }
+    return arr
+}
+console.log(bubbleSort([1, 9, 4, 7, 5, 8, 6]));
